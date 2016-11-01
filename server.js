0090f76c07f917af0ea2d324a21543634faa59ca
zjmgmj <943257156@qq.com>
2016-11-01 14:43:50 +0800

    1

---
+++ server.js
@@ -0,0 +1,67 @@
+/*var express = require('express');
+ var path = require('path');
+ var favicon = require('serve-favicon');
+ var logger = require('morgan');
+ var cookieParser = require('cookie-parser');
+ var bodyParser = require('body-parser');
+
+ var routes = require('./routes/index');
+ var users = require('./routes/users');
+
+ var app = express();*/
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    mime = require('mime');
+var cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-type': 'text/plain'});
+    response.write('error 404:resource not found.');
+    response.end();
+}
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {"Content-type": mime.lookup(path.basename(filePath))});
+    response.end(fileContents)
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+                fs.readFile(absPath, function (err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        })
+    }
+}
+
+var server = http.createServer(function (request, response) {
+    var filePath = false;
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function () {
+    console.log("server listening on port 3000");
+});
+
+
+var chatServer;
+chatServer = require('./lib/chat_server');
+chatServer.listen(server);
